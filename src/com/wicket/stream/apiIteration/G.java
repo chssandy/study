89e186f9b162a503b7588e1af1681f45cabcb6d8
chs_sandy@163.com <chs_sandy@163.com>
2021-01-04 14:43:19 +0800

    gitignore

---
+++ src/com/wicket/stream/apiIteration/G.java
@@ -0,0 +1,16 @@
+package com.wicket.stream.apiIteration;
+
+/**
+ * @param
+ * @author CHS
+ * @version 1.0
+ * @copyright WICKET.COM
+ * @classname G
+ * @description TODO
+ * @date 2020/9/24 10:58
+ */
+public interface G {
+    default void hello(){
+        System.out.println("Hello from G");
+    }
+}
